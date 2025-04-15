5a4803c37bbfd602550ad22005db7c7582c2fdaa
NATALY JESSICA RIBEIRO DE SOUZA SOUZA <u21446@dinalva44.academico.cotuca.unicamp.br>
2025-04-15 17:30:25 -0300

    comentando sobre leitura e montagem do labirinto -nataly

--- Program/Labirinto/LeitorDeTXT.java
+++ Program/Labirinto/LeitorDeTXT.java
@@ -1,3 +1,28 @@
 public class LeitorDeTXT {
     
+    //pegar caminho do arquivo
+    //ler arquivo
+    //pegar linhas e colunas
+    //gerar tamanho da matriz
+    // percorrer arquivo e adicionar caracteres 
+    /*
+    - # parede
+    - E entrada
+    - S saida
+    - " " caminho 
+    */ 
+
+    public String Ler_Coluna_Linhas( String caminhoArquivo){
+        String lido = caminhoArquivo; 
+        return lido;
+    }
+
+    public montar_matriz(){
+
+    }
+
+    public preencher_labirinto(){
+        
+    } 
+
 }
