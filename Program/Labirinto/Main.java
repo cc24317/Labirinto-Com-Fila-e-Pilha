869a51c35825d2a47946f9774d0e834c0de9c8a6
NATALY JESSICA RIBEIRO DE SOUZA SOUZA <u21446@dinalva44.academico.cotuca.unicamp.br>
2025-04-15 17:09:46 -0300

    classes pilha e fila funcionando

--- Program/Labirinto/Main.java
+++ Program/Labirinto/Main.java
@@ -1,3 +1,57 @@
-public class Main {
-    
+/*public class Main
+{
+	public static void main(String[] args)
+	{
+	    Pilha<String> pil = new Pilha<String> ();
+	    
+	    try
+	    {
+    	    pil.guardeUmItem("PYTHON");
+    	    pil.guardeUmItem("C");
+    	    pil.guardeUmItem("JAVA");
+    	    pil.guardeUmItem("C++");
+    	    pil.guardeUmItem("HTML");
+    	    
+    	    System.out.println (pil.recupereUmItem());
+    	    pil.removaUmItem   ();
+    	    System.out.println (pil.recupereUmItem());
+    	    pil.removaUmItem   ();
+    	    System.out.println (pil.recupereUmItem());
+    	    pil.removaUmItem   ();
+	    }
+	    catch (Exception erro)
+	    {
+	        System.err.println (erro.getMessage());
+	    }
+	}
+}*/
+
+/* 
+public class Main
+{
+	public static void main(String[] args)
+	{
+	    Fila<String> fil = new Fila<String> ();
+	    
+	    try
+	    {
+    	    fil.guardeUmItem("PYTHON");
+    	    fil.guardeUmItem("C");
+    	    fil.guardeUmItem("JAVA");
+    	    fil.guardeUmItem("C++");
+    	    fil.guardeUmItem("HTML");
+    	    
+    	    System.out.println (fil.recupereUmItem());
+    	    fil.removaUmItem   ();
+    	    System.out.println (fil.recupereUmItem());
+    	    fil.removaUmItem   ();
+    	    System.out.println (fil.recupereUmItem());
+    	    fil.removaUmItem   ();
+	    }
+	    catch (Exception erro)
+	    {
+	        System.err.println (erro.getMessage());
+	    }
+	}
 }
+*/
